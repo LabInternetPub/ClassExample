c1f0e3caaef87f7a1207519b7f1493fc513deaaa
Josep Roure Alcobé <roure@tecnocampus.cat>
2018-10-17 12:51:24 +0200

    typo correction

--- src/main/java/cat/tecnocampus/webclassexample/configuration/security/MemorySecurityConfiguration.java
+++ src/main/java/cat/tecnocampus/webclassexample/configuration/security/MemorySecurityConfiguration.java
@@ -27,7 +27,7 @@ public class MemorySecurityConfiguration extends BaseSecurityConfiguration {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("alvarez").password("'{bcrypt}$2a$10$7PFxXn4TQRiut9jNcAl7AubQZUWWck/eML3TDaQtoZiWNEN6o.Ig6").roles("USER").and()
+                .withUser("alvarez").password("{bcrypt}$2a$10$7PFxXn4TQRiut9jNcAl7AubQZUWWck/eML3TDaQtoZiWNEN6o.Ig6").roles("USER").and()
                 .withUser("roure").password("{bcrypt}$2a$10$0VGzG8lfiDXBnFTE98lfiOLtP4uh62wnE6iWs5.2AMrJ3G9k7XZqu").roles("ADMIN").and()
                 .withUser("lecina").password("{bcrypt}$2a$10$8J6t9dJ7KCU1KGBd7E2/2enlG1tyNkzoswFbckO01Efwp6wF6MH7u").roles("USER,ADMIN");
     }
